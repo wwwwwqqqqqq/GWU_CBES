ab9515aff7243638b6888bf9675ba91d77f0da27
ThomasRStanton <thomasrstantonjr@gmail.com>
2024-03-01 16:56:28 -0500

    Added alphanumeral character check

--- preprocess_text.py
+++ preprocess_text.py
@@ -1,6 +1,6 @@
 """
 File that provides functionality for Preprocessing string data for keyword search.
-Preprocessing includes: normalization of text, tokenization of text, removal of stop word tokens, lemmatization and stemming of tokens.
+Preprocessing includes: normalization of text(capitol and non-text), tokenization of text, removal of stop word tokens, lemmatization and stemming of tokens.
 
 Written by: Thomas Stanton
 """
@@ -9,6 +9,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
+import re
 
 # Download necessary NLTK resources
 nltk.download('punkt')
@@ -22,10 +23,14 @@ def preprocess_text(text):
 
     :param text: A string text to be tokenized.
     :return: A list of strings that can be combined to represent the text without stopwords,
-    stems, uppercase letters, and where the words are lemmatized if possible.
+    stems, uppercase letters, non-alphabet chars, and where the words are lemmatized if possible.
     """
     # Normalization: convert to lowercase
     text = text.lower()
+
+    # Non-Alpha Removal: remove non alphabet characters
+    text = re.sub(r'[^0-9a-zA-Z ]+', '', text)
+    print(text)
 
     # Tokenization: split the text into words
     tokens = word_tokenize(text)
@@ -45,6 +50,6 @@ def preprocess_text(text):
 
 if __name__ == '__main__':
     # Example usage
-    example_text = "The quick brown fox jumps over the lazy dog"
+    example_text = "TensorFlow is an end-to-end open source platform for machine learning. It has a comprehensive, flexible ecosystem of tools, libraries, and community resources that lets researchers push the state-of-the-art in ML and developers easily build and deploy ML-powered applications."
     processed_text = preprocess_text(example_text)
     print(processed_text)
